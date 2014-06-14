b9c66d4c88532c3ad09d72ed2c304b6fb69ddae6
Ben Woosley <ben.woosley@gmail.com>
2014-06-13 20:31:34 -0700

    Drop tasks - we don't need them.

--- lib/pdfjs-rails.rb
+++ lib/pdfjs-rails.rb
@@ -3,9 +3,6 @@ require "pdfjs-rails/version"
 module Pdfjs
   module Rails
     class Engine < ::Rails::Engine
-      rake_tasks do
-        load "pdfjs-rails/tasks.rake"
-      end
     end
   end
 end
